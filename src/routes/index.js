559150601c32d31a0be936ff02ada49491e74f2d
Ignacio Saurin <nachosaurin@gmail.com>
2023-02-23 18:08:31 -0300

    Backend ready. Simple Authorization Backend.

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = Router();
 const User = require('../models/User');
 
 const jwt = require('jsonwebtoken');
-const e = require('express');
+//const e = require('express');
 
 router.get('/', (req, res) => res.send('Hello World'))
 
@@ -21,8 +21,8 @@ router.post('/signup', async (req, res)=>{
 
 router.post('/signin', async (req, res) => {
    const {email, password} = req.body;
+   
    const user = await User.findOne({email})
-
    if (!user) return res.status(401).send("The email doesn't exist!");
    if (user.password !== password) return res.status(401).send("Wrong Password!");
 
@@ -30,5 +30,71 @@ router.post('/signin', async (req, res) => {
    return res.status(200).json({token});
 })
 
+router.get('/tasks', (req, res) => {
+    res.json ([
+        {
+        _id: 1,
+        name:'Task one',
+        description: "Hola como estas. Tarea 1",
+        date: "12/12/2003"
+    },
+    {
+        _id: 2,
+        name:'Task two',
+        description: "Hola como estas. Tarea 1",
+        date: "12/12/2003"
+    },
+    {
+
+        _id: 3,
+        name:'Task three',
+        description: "Hola como estas. Tarea 1",
+        date: "12/12/2003"
+    }
+])
+})
+
+router.get('/private-tasks', verifyToken, (req, res) =>{
+    res.json ([
+        {
+        _id: 1,
+        name:'Task one',
+        description: "Hola como estas. Tarea 1",
+        date: "12/12/2003"
+    },
+    {
+        _id: 2,
+        name:'Task two',
+        description: "Hola como estas. Tarea 1",
+        date: "12/12/2003"
+    },
+    {
+
+        _id: 3,
+        name:'Task three',
+        description: "Hola como estas. Tarea 1",
+        date: "12/12/2003"
+    }
+])
+})
+
+router.get('/profile', verifyToken, (req, res) => {
+    res.send(req.userId);
+})
+
 module.exports = router;
 
+function verifyToken (req, res, next){
+    if (!req.headers.authorization){
+        return res.status(401).send('Unauthorize Request');
+    }
+    const token = req.headers.authorization.split(' ')[1]
+    if (token === 'null') {
+        return res.status(401).send('Unauthorize Request');
+    }
+
+    const payload = jwt.verify(token, 'secretkey')
+    req.userId = payload._id;
+    next();
+    
+}
