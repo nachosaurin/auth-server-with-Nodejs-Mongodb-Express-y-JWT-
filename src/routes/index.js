ac80d470b9f1498b058bae385c8b9120c42834b3
Ignacio Saurin <nachosaurin@gmail.com>
2023-02-23 16:56:23 -0300

    Sign-In and Sign-Up working

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,31 @@ const router = Router();
 
 const User = require('../models/User');
 
+const jwt = require('jsonwebtoken');
+const e = require('express');
+
 router.get('/', (req, res) => res.send('Hello World'))
 
 router.post('/signup', async (req, res)=>{
     const { email, password } = req.body;
     const newUser = new User ({email, password});
     await newUser.save();
-    res.send('user registered');
+    
+    const token = jwt.sign({ _id: newUser._id }, 'secretkey')
+    res.status(200).json({token})
+
+})
+
+
+router.post('/signin', async (req, res) => {
+   const {email, password} = req.body;
+   const user = await User.findOne({email})
+
+   if (!user) return res.status(401).send("The email doesn't exist!");
+   if (user.password !== password) return res.status(401).send("Wrong Password!");
+
+   const token = jwt.sign({ _id: user._id}, 'secretkey');
+   return res.status(200).json({token});
 })
 
 module.exports = router;
